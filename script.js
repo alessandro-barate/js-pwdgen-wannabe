7ac93c1af70f9aff84cd0d0d349e71077ab497ca
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-04-23 16:58:34 +0200

    [UPD] Other console instructions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(document.getElementById('user-identity').innerHTML);
+// Istruzione solo per vedere come funziona console.log
+// console.log(document.getElementById('user-identity').innerHTML);
+
+const userName = prompt('Digita il tuo nome');
+const userSurname = prompt('Digita il tuo cognome');
+
+// Istruzioni per vedere se vengono mostrare in console le variabili
+// console.log(userName);
+// console.log(userSurname);
+
+
