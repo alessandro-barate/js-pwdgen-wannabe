dd6782a194f82dc8653234d414ddddd0308cff86
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-04-23 17:18:40 +0200

    [UPD] Number added to the final variable

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// Istruzione solo per vedere come funziona console.log
-// console.log(document.getElementById('user-identity').innerHTML);
+let currentText = document.getElementById('user-identity').innerHTML;
+console.log(document.getElementById('user-identity').innerHTML);
 
 const userName = prompt('Digita il tuo nome');
+console.log(userName);
+
 const userSurname = prompt('Digita il tuo cognome');
+console.log(userSurname);
 
-// Istruzioni per vedere se vengono mostrare in console le variabili
-// console.log(userName);
-// console.log(userSurname);
+const userFavouriteColor = prompt('Digita il tuo colore preferito');
+console.log(userFavouriteColor);
 
+currentText = `${currentText} ${userName} ${userSurname} ${userFavouriteColor} 24`;
+
+document.getElementById('user-identity').innerHTML = currentText;
+console.log(currentText);
 
